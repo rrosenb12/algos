b11fa9787cf1d6905d61b168b207b78bdfdf85c4
rrosenb12 <rebecca.ap.rosenberg@gmail.com>
2022-05-28 20:02:40 -0400

    reverse each word ruby

---
+++ ruby/reverse_each_word.rb
@@ -0,0 +1,13 @@
+# Given a string, reverse each word in the sentence
+# const string = "Welcome to this Javascript Guide!";
+# return `emocleW ot siht tpircsavaJ !ediuG`
+
+def ReverseEachWord(sentence)
+    reversed_sentence_array = []
+    for word in sentence.split(" ") do
+        reversed_sentence_array << word.reverse()
+    end
+    return (reversed_sentence_array.join(" "))
+end
+
+puts ReverseEachWord("Welcome to this Javascript Guide!")
