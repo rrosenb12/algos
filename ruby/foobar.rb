71c8f743738a1ac44b90a7fb9596ab5100d9994f
rrosenb12 <rebecca.ap.rosenberg@gmail.com>
2022-05-30 12:10:49 -0400

    refactoring ruby fizz buzz to be more ruby

--- ruby/foobar.rb
+++ ruby/foobar.rb
@@ -1,7 +1,7 @@
 # Write a function called FooBar that takes input integer n and prints all the numbers from 1 up to n in a new line. If the number is divisible by 3 then print "Foo", if the number is divisible by 5 then print "Bar" and if the number is divisible by both 3 and 5, print "FooBar". Otherwise just print the number.
 
 def FooBar(n)
-    for numb in (1..n).each do 
+    (1..n).each do |numb|
         if numb % 3 == 0 and numb % 5 == 0
             puts "FooBar"
         elsif numb % 3 == 0
